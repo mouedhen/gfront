16320ffb87c6ea1af2c1b8903a4963e73ae07014
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2018-03-21 11:05:03 +0100

    add coming soon to missing page

--- pages/_lang/reports/index.vue
+++ pages/_lang/reports/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="container">
-    coming soon
+    <div id="p-content-undeconstruction">
+      <div id="container">
+        <img src="https://pbs.twimg.com/profile_images/459505368234606592/nuxaaQoo_400x400.jpeg" id="avatar"
+             width="250" draggable="false"/>
+
+        <div id=text>
+          <h1>Building…</h1>
+          <p title="(I guess)">(coming soon)</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
