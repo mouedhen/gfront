f48fe24fb28344b664675a8c108f4a3e115a7bf1
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2018-03-21 03:24:21 +0100

    beta-0.5.1

--- models/config.js
+++ models/config.js
@@ -1,4 +1,8 @@
-export const apiDomain = 'http://localhost:8000/api/v1';
-export const appDomain = 'http://localhost:3000';
-export const storageDomain = 'http://localhost:8000';
+// export const apiDomain = 'http://localhost:8000/api/v1';
+// export const appDomain = 'http://localhost:3000';
+// export const storageDomain = 'http://localhost:8000';
+
+export const apiDomain = 'https://www.goulelhom.org/api/v1';
+export const appDomain = 'http://211.ip-92-222-83.eu:3000';
+export const storageDomain = 'https://www.goulelhom.org';
 
