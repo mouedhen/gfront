4993847ccc1286314109c59a088088e316e0c3e3
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2018-03-21 04:01:20 +0100

    beta-0.5.3

--- models/config.js
+++ models/config.js
@@ -2,7 +2,7 @@
 // export const appDomain = 'http://localhost:3000';
 // export const storageDomain = 'http://localhost:8000';
 
-export const apiDomain = 'https://www.goulelhom.org/api/v1';
-export const appDomain = 'http://admin.goulelhom.org';
-export const storageDomain = 'https://www.goulelhom.org';
+export const apiDomain = 'https://admin.goulelhom.org/api/v1';
+export const appDomain = 'https://www.goulelhom.org';
+export const storageDomain = 'https://admin.goulelhom.org';
 
