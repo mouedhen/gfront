affbe323a7c213974b2ff2cd84035c918702c8cd
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2018-04-26 04:37:47 +0100

    fix index

--- models/config.js
+++ models/config.js
@@ -1,5 +1,5 @@
-export const apiUrl = 'http://localhost:8000/api/v1/public/';
-// export const apiUrl = 'https://dashboard.goulelhom.org/api/v1/public/';
+// export const apiUrl = 'http://localhost:8000/api/v1/public/';
+export const apiUrl = 'https://dashboard.goulelhom.org/api/v1/public/';
 // export const appDomain = 'http://localhost:3000';
 // export const storageDomain = 'http://localhost:8000';
 
